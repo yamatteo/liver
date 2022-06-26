2c6691ad4926f8b4f822b662b8196e8e74c4353e
yamatteo@gmail.com <yamatteo@gmail.com>
2022-06-26 15:38:16 +0200

    modified training cycle

--- train.py
+++ train.py
@@ -36,6 +36,21 @@ def train_batch_step(scan: ScanBatch,
     report.append(info_items)
     return info_items["train"], batch_losses_items
 
+def valid_batch_step(scan: ScanBatch,
+                     segm: FloatSegmBatch, *,
+                     model: UNet,
+                     keys: list[int]
+                     ) -> tuple[float, dict[int, float]]:
+    model.eval()
+    pred = model.forward(scan)
+    batch_losses, info_items = pred.distance_from(segm)  # TODO change to generic distance
+    batch_losses_items = {k: batch_losses[i].item() for i, k in enumerate(keys)}
+
+    loss = torch.sum(batch_losses)
+    info_items["train"] = loss.item() / len(batch_losses)
+    report.append(info_items)
+    return info_items["train"], batch_losses_items
+
 
 def valid_step(scan: ScanBatch, segm: FloatSegmBatch, *, model: UNet, keys: list[int]) \
         -> tuple[float, dict[int, float]]:
@@ -147,16 +162,69 @@ def train_cycle(model, *,
     for epoch in range(epochs):
         if epoch % 20 == 0:
             # Evaluation
-            losses = eval_step(
-                model,
-                train_dataset=train_dataset,
-                valid_dataset=valid_dataset,
-                optimizer=optimizer,
-                device=device,
-                batch_size=batch_size,
-                epochs=epochs,
-                epoch=epoch
-            )
+            epoch_loss = 0
+            losses = {}
+
+            with Progress() as progress:
+                task = progress.add_task(
+                    f"Epoch {epoch + 1}/{epochs}. Loss so far: {epoch_loss:.2e}".ljust(50, ' '),
+                    total=len(valid_dataset)
+                )
+                for keys, scan_segm in valid_dataset.batches(batch_size):
+                    scan, segm = scan_segm.separate()
+                    scan = scan.to(device=device, dtype=torch.float32)
+                    segm = segm.to(device=device, dtype=torch.float32)
+
+                    loss, batch_losses = valid_batch_step(
+                        scan,
+                        segm,
+                        model=model,
+                        keys=keys
+                    )
+
+                    epoch_loss += loss
+                    progress.update(
+                        task,
+                        advance=batch_size,
+                        description=f"Epoch {epoch + 1}/{epochs}. Loss so far: {epoch_loss:.2e}".ljust(50, ' ')
+                    )
+                progress.update(
+                    task,
+                    description=(f"Epoch {epoch}/{epochs}. "
+                                 f"Validation total loss: {epoch_loss:.2e}. "
+                                 f"Loss per scan: {epoch_loss / len(valid_dataset):.2e}").ljust(50, ' ')
+                )
+
+            with Progress() as progress:
+                task = progress.add_task(
+                    f"Epoch {epoch + 1}/{epochs}. Loss so far: {epoch_loss:.2e}".ljust(50, ' '),
+                    total=len(train_dataset)
+                )
+                for keys, scan_segm in train_dataset.batches(batch_size):
+                    scan, segm = scan_segm.separate()
+                    scan = scan.to(device=device, dtype=torch.float32)
+                    segm = segm.to(device=device, dtype=torch.float32)
+
+                    loss, batch_losses = valid_batch_step(
+                        scan,
+                        segm,
+                        model=model,
+                        keys=keys
+                    )
+
+                    epoch_loss += loss
+                    losses.update(batch_losses)
+                    progress.update(
+                        task,
+                        advance=batch_size,
+                        description=f"Epoch {epoch + 1}/{epochs}. Loss so far: {epoch_loss:.2e}".ljust(50, ' ')
+                    )
+                progress.update(
+                    task,
+                    description=(f"Epoch {epoch}/{epochs}. "
+                                 f"Training total loss: {epoch_loss:.2e}. "
+                                 f"Loss per scan: {epoch_loss / len(train_dataset):.2e}").ljust(50, ' ')
+                )
             smallest = heapq.nsmallest(buffer_size, list(losses.keys()), lambda k: losses[k])
             sub_dataset = train_dataset.subset(smallest)
             dataloader = DataLoader(

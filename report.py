bbbf5eaeba0b32e42f456a0925c96ce630f026a3
yamatteo@gmail.com <yamatteo@gmail.com>
2022-07-10 17:37:33 +0200

    Debug: image generation

--- report.py
+++ report.py
@@ -41,9 +41,9 @@ def __append(items: dict):
 def __sample(scan: FloatScanBatch, pred: FloatSegmBatch, segm: FloatSegmBatch):
     n = random.randint(0, scan.size("N") - 1)
     z = random.randint(0, scan.size("Z") - 1)
-    image = get_white(scan, n=n, z=z).unsqueeze(2).numpy()
-    liver_image = rgb_sample(scan, pred, segm, mode=("liver_error", "liver", "background"), n=n, z=z, data_format="HWC")
-    tumor_image = rgb_sample(scan, pred, segm, mode=("tumor_error", "tumor", "background"), n=n, z=z, data_format="HWC")
+    image = get_white(scan, n=n, z=z).unsqueeze(2).numpy() / 255
+    liver_image = rgb_sample(scan, pred, segm, mode=("liver_error", "liver", "background"), n=n, z=z, data_format="HWC") / 255
+    tumor_image = rgb_sample(scan, pred, segm, mode=("tumor_error", "tumor", "background"), n=n, z=z, data_format="HWC") / 255
     class_labels = {
         0: "background",
         1: "liver",
